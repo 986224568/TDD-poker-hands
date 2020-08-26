58960225341bf75d33776229ffa1ccfbd07ebc78
Domingo <986224568@qq.com>
2020-08-26 14:21:46 +0800

    [poker-hands][Sam, Domingo] test should_return_true_with_player2_win_when_playing_porkerHands_compare_by_poker_value_and_character_given_2_player_cards

--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -26,4 +26,16 @@ public class DemoTest {
         //then
         assertEquals(2,result);
     }
+
+    @Test
+    void should_return_true_with_player2_win_when_playing_porkerHands_compare_by_poker_value_and_character_given_2_player_cards() {
+        //given
+        String[] player1 = {"AS", "3D", "4S", "5C", "7H"};
+        String[] player2 = {"AH", "3S", "4D", "5S", "7D"};
+        //when
+        int result = pokerHands.play(player1, player2);
+        //then
+        assertEquals(1,result);
+    }
+
 }
