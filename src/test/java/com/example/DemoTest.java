941f620c65992f0e9cb27a536a88328cbe15d002
Domingo <986224568@qq.com>
2020-08-26 12:44:35 +0800

    [poker-hands][Sam, Domingo] bug-fix HA replace H1

--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -16,10 +16,10 @@ public class DemoTest {
     }
 
     @Test
-    void should_return_play1Win_when_playing_porkerHands_given_2_player_cards() {
+    void should_return_true_with_player2_win_when_playing_porkerHands_given_2_player_cards() {
         //given
         String[] player1 = {"H2", "D3", "S4", "C5", "H7"};
-        String[] player2 = {"H1", "S3", "D4", "S5", "D7"};
+        String[] player2 = {"HA", "S3", "D4", "S5", "D7"};
         //when
         int result = pokerHands.play(player1, player2);
         //then
