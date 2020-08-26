be0d23b20a6aa55ab28397d121e489e2aff7c3ee
Domingo <986224568@qq.com>
2020-08-26 12:34:55 +0800

    [poker-hands][Sam, Domingo] test: should_return_play1Win_when_playing_porkerHands_given_2_player_cards

---
+++ src/main/java/com/example/PokerHands.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class PokerHands {
+
+    public int play (String[] play1, String[] play2) {
+        return 1;
+    }
+}
--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -1,21 +1,28 @@
 package com.example;
 
-import org.junit.jupiter.api.BeforeAll;
+import
+        org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DemoTest {
-    private static Demo demo;
+    private static PokerHands pokerHands;
 
     @BeforeAll
     public static void setUp() throws Exception {
-        demo = new Demo();
+        pokerHands = new PokerHands();
     }
 
     @Test
-    public void test01() {
-        String result = demo.run();
-        assertThat(result).isEqualTo("ABC");
+    void should_return_play1Win_when_playing_porkerHands_given_2_player_cards() {
+        //given
+        String[] player1 = {"H2", "D3", "S4", "C5", "H7"};
+        String[] player2 = {"H1", "S3", "D4", "S5", "D7"};
+        //when
+        int result = pokerHands.play(player1, player2);
+        //then
+        assertEquals(2,result);
     }
 }
