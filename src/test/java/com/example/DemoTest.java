ea8ab21f11d7783903d11cee4a639c1f8d2fc839
Domingo <986224568@qq.com>
2020-08-26 14:30:15 +0800

    [poker-hands][Sam, Domingo] test should_return_true_with_player2_win_when_playing_porkerHands_compare_by_poker_value_and_character_and_pair_given_2_player_cards

--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -24,18 +24,28 @@ public class DemoTest {
         //when
         int result = pokerHands.play(player1, player2);
         //then
-        assertEquals(2,result);
+        assertEquals(2, result);
     }
 
     @Test
-    void should_return_true_with_player2_win_when_playing_porkerHands_compare_by_poker_value_and_character_given_2_player_cards() {
+    void should_return_true_with_player1_win_when_playing_porkerHands_compare_by_poker_value_and_character_given_2_player_cards() {
         //given
         String[] player1 = {"AS", "3D", "4S", "5C", "7H"};
         String[] player2 = {"AH", "3S", "4D", "5S", "7D"};
         //when
         int result = pokerHands.play(player1, player2);
         //then
-        assertEquals(1,result);
+        assertEquals(1, result);
     }
 
+    @Test
+    void should_return_true_with_player2_win_when_playing_porkerHands_compare_by_poker_value_and_character_and_pair_given_2_player_cards() {
+        //given
+        String[] player1 = {"AS", "3D", "4S", "5C", "7H"};
+        String[] player2 = {"AH", "3S", "AD", "5S", "7D"};
+        //when
+        int result = pokerHands.play(player1, player2);
+        //then
+        assertEquals(2, result);
+    }
 }
